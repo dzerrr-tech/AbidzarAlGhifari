d56345eb0c34ffaa9b68b16599a36c74d56ccafe
dzerrr-tech <hutreon@gmail.com>
2026-09-19 13:41:59 +0700

    final

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsConfigPaths from "vite-tsconfig-paths";
+import { nitro } from "nitro/vite";
 
 export default defineConfig({
   resolve: {
@@ -18,6 +19,7 @@ export default defineConfig({
     tanstackStart({
       server: { entry: "server" },
     }),
+    nitro(),
     viteReact(),
   ],
 });
